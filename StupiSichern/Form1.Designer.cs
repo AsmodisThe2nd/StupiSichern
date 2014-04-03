755a8b8c02216f468a0dab7e3f4fa2813f706253
AsmodisThe2nd <asmodis94@web.de>
2014-04-03 23:27:00 +0200

    initial

---
+++ StupiSichern/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace StupiSichern
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(794, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Starten";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(188, 13);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gleichzeitig zu bearbeitende Seiten:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Benutzername:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Passwort:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(262, 77);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Log in-Info";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Starte mit Seitentitel:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(188, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "A";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(280, 1);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(314, 77);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Einstellungen";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(644, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Last:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(599, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "<<";
+            this.toolTip1.SetToolTip(this.button2, "Letzten Artikelnamen als neuen Startartikel übernehmen");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 113);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(794, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Firebrick;
+            this.label6.Location = new System.Drawing.Point(386, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(644, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Namespace:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 145);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "   SaveStupidedia";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ StupiSichern/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Xml;
+using DotNetWikiBot;
+
+namespace StupiSichern
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string lastarticle;
+        private List<string> errors = new List<string> { };
+       private bool sw = false;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sw = !sw;
+            if (sw)
+            {
+                if (textBox1.Text != "" && textBox2.Text != "")
+                {
+
+                    progressBar1.Maximum = (int)numericUpDown1.Value;
+                    backgroundWorker1.RunWorkerAsync();
+                    button1.Text = "Abbrechen";
+                    textBox1.ReadOnly = true;
+                    textBox2.ReadOnly = true;
+                }
+                else
+                {
+                    sw = !sw;
+                }
+            }
+            else
+            {
+                backgroundWorker1.CancelAsync();
+                button1.Text = "Starten";
+                textBox1.ReadOnly = false;
+                textBox2.ReadOnly = false;
+            }
+            
+        }
+
+
+        private static string RemoveIllegalCharacters(string fileName)
+        {
+            string illegal = fileName;
+            string invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+
+            foreach(char c in invalid)
+            {
+                illegal = illegal.Replace(c.ToString(), "");
+            }
+
+            return illegal;
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = lastarticle;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int num = 0;
+            try
+            {
+                backgroundWorker1.ReportProgress(-1, new String[] { "Logging in" });
+                Site site = new Site("http://www.stupidedia.org", textBox1.Text, textBox2.Text);
+                backgroundWorker1.ReportProgress(-1, new String[] { "Done" });
+                if (backgroundWorker1.CancellationPending)
+                    return;
+                backgroundWorker1.ReportProgress(-1, new String[] { "Fetching " + (int)numericUpDown1.Value + " Pages" });
+                PageList pl = new PageList(site);
+                pl.FillFromAllPages(textBox3.Text, 0, true, (int)numericUpDown1.Value);
+                backgroundWorker1.ReportProgress(-1, new String[] { "Done" });
+                foreach (Page p in pl)
+                {
+                    if (backgroundWorker1.CancellationPending)
+                        return;
+                    try
+                    {
+                        p.Load();
+
+                        lastarticle = p.title;
+                        string ns = "";
+                        if (!site.namespaces.TryGetValue(p.GetNamespace(), out ns))
+                        {
+                            ns = "Artikelnamensraum";
+                        }
+                        ns = RemoveIllegalCharacters(ns);
+                        if (!Directory.Exists("Articles\\" + ns))
+                        {
+
+                            Directory.CreateDirectory("Articles\\" + ns);
+                        }
+                        p.SaveToFile(Path.Combine("Articles\\" + ns, RemoveIllegalCharacters(p.title) + ".txt"));
+                        backgroundWorker1.ReportProgress(num, new String[] { p.title, ns });
+                        num += 1;
+                    }
+                    catch (Exception ex2)
+                    {
+                        backgroundWorker1.ReportProgress(-2, new String[] { p.title});
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage >= 0)
+            {
+                lastarticle = ((string[])e.UserState)[0];
+                progressBar1.Value = e.ProgressPercentage;
+                label6.Text = e.ProgressPercentage + "/" + numericUpDown1.Value.ToString();
+                label5.Text = "Last: " + lastarticle;
+                label7.Text = "Namespace: " + ((string[])e.UserState)[1];
+
+            }
+            else if(e.ProgressPercentage == -1)
+            {
+                label6.Text = ((string[])e.UserState)[0];
+            }
+            else if (e.ProgressPercentage == -2)
+            {
+                errors.Add(((string[])e.UserState)[0]);
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Value = 0;
+            label6.Text = "";
+            label7.Text = "Namespace:";
+            if(!(errors.Count == 0)){
+                File.Delete("Articles\\errors.txt");
+                File.Create("Articles\\errors.txt");
+                File.WriteAllLines("Articles\\errors.txt", errors.ToArray());
+                MessageBox.Show("Es gab Fehler während des Speicherns. Die betroffenen Artikel sind unter \"Articles\\errors.txt\" nachzulesen");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           DialogResult dr =  MessageBox.Show("Soll der zuletzt geladene Artikel gespeichert werden, damit später fortgefahren werden kann?", "Speichern?", MessageBoxButtons.YesNo);
+           if (dr == System.Windows.Forms.DialogResult.Yes)
+           {
+               Properties.Settings.Default.last = lastarticle;
+           }
+           Properties.Settings.Default.Username = textBox1.Text;
+           Properties.Settings.Default.Password = textBox2.Text;
+           Properties.Settings.Default.Number = (int)numericUpDown1.Value;
+           Properties.Settings.Default.Save();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Properties.Settings.Default.Reload();
+
+            textBox3.Text = Properties.Settings.Default.last;
+            textBox1.Text = Properties.Settings.Default.Username;
+            textBox2.Text = Properties.Settings.Default.Password;
+            numericUpDown1.Value = (decimal)Properties.Settings.Default.Number;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
